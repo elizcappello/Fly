ec5bc60613b74b8425924b517af1ff2fd02979fe
elizcappello <lizc@elizabeellosmbp.goucher.edu>
2016-07-25 09:06:58 -0400

    fly

---
+++ sketch.js
@@ -0,0 +1,29 @@
+
+
+
+
+  function preload(){
+  fly = loadImage("assets/fly.jpg")
+  //image name
+}
+
+function setup() {
+  createCanvas(1200,700)
+  frameRate(60)
+  noStroke()
+  fill(255)
+}
+
+function draw() {
+  background(0,204,255)
+  image(fly,mouseX,mouseY,100,100)
+
+   //image name, mouse anywhere, size
+  
+}
+  
+  
+  
+ 
+
+
